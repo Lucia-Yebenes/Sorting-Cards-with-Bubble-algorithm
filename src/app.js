dec598f01fef22631c7ec89a67c09b1b3e9e24b2
Lucia Yebenes <yebeneslucia@gmail.com>
2021-04-26 19:51:30 +0000

    casi q se termina

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ window.onload = function() {
   //creo las array con los distintos simbolos y numeros
   let simbolo = ["♦", " ♥", "♠", "♣"];
   let Numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "K", "Q", "J"];
+  let mazo = [];
   //para obtener carta
   //1ro llamo al elemento el div donde esta el elemento y lo guardo en variable
   let CartaAux = document.querySelector(".cartaDeso");
@@ -34,8 +35,6 @@ window.onload = function() {
     //creo variable contador entonces le digo cuendo el numero que ingresa en la variable Num card sea
     //menor a contador entonces imprimir carta y luedo sumo uno
     let contador = 0;
-    var OrdenAux = []; //declaro afuera de while, para que no me cree muchas
-    var PalosRandom = [];
     while (contador < NumCard) {
       //creo estas vatiables , y las igualo a lo que me devuelve la fuuncion randon de los array simbolo
       //y del array numero.
@@ -44,40 +43,39 @@ window.onload = function() {
       //llamo a la primera funcion y le paso los parametros y las crea
       CrearCarta(CardPalo, CardNum);
       contador++;
-      //me devuelve la array con todos los numeros random
-      OrdenAux[contador] = CardNum;
-      PalosRandom[contador] = CardPalo;
+      //concateno array me queda una array dentro de la otra no una traz de la otra
+      mazo.push([CardNum, CardPalo]);
     }
-    console.log(OrdenAux); //impriime array de nuemros
-    console.log(PalosRandom); // imprime array de simbolos
+
     //lista con solo nuemros, para poder ordenar las cartas paso las letras a nuemros
     var i;
-    for (i = 0; i <= OrdenAux.length - 1; i++) {
-      switch (OrdenAux[i]) {
+    for (i = 0; i <= mazo.length - 1; i++) {
+      switch (mazo[i][0]) {
         case "A":
-          OrdenAux[i] = 1;
+          mazo[i][0] = 1;
           break;
         case "K":
-          OrdenAux[i] = 10;
+          mazo[i][0] = 10;
           break;
         case "Q":
-          OrdenAux[i] = 11;
+          mazo[i][0] = 11;
           break;
         case "J":
-          OrdenAux[i] = 12;
+          mazo[i][0] = 12;
           break;
         default:
         // code block
       }
     }
-    console.log(OrdenAux);
+    //console.log(OrdenAux);
+    //[[1,corazon],[4,diamante],[8,pica]]
     const bubbleSort = arr => {
       let wall = arr.length - 1; //iniciamos el wall o muro al final del array
       while (wall > 0) {
         let index = 0;
         while (index < wall) {
           //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
-          if (arr[index] > arr[index + 1]) {
+          if (arr[index][0] > arr[index + 1][0]) {
             let aux = arr[index];
             arr[index] = arr[index + 1];
             arr[index + 1] = aux;
@@ -88,18 +86,7 @@ window.onload = function() {
       }
       return arr;
     };
-    console.log(bubbleSort(OrdenAux)); //lista ordenada lo mas del mundo mundial
+    console.log(bubbleSort(mazo)); //lista ordenada
+    console.log(mazo);
   });
 };
-
-//para obtener ordenada
-//1ro llamo al elemento el div donde esta el elemento y lo guardo en variable
-/*let CartaAux = document.querySelector(".CartaOrden");
-  function CrearCarta(CardPalo, NumOrden) {
-    //funcion devuelve cata creo vatiables palo y numero para poder almacenar random
-    CartaAux.innerHTML += `<div class="Carta col-10 bg-light rounded-lg p-0 mx-5 my-5 shadow-lg p-3 mb-5 bg-white rounded">
-                    <div class="icono1 col-12 text-right px-0">${CardPalo}</div>
-                    <h1 id="num" class="text-center">${NumOrden}</h1>
-                    <div id="IconROtado" class="icono1 col-12 px-0">${CardPalo}</div>
-                </div>`;
-  }*/
